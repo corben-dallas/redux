afe276063413b40df639451666918e40a7ea6135
Corbens <albert.u1994@gmail.com>
2020-10-17 17:56:47 +0300

    Updates

---
+++ precache-manifest.009f26e78cfaf8168ba6eacfcba78af2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f55593a340c9067277d57333ca4ad7b",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "9d5524a1f2638cad7284",
+    "url": "/redux/static/css/main.0e644737.chunk.css"
+  },
+  {
+    "revision": "4e48e2b024065e125b3a",
+    "url": "/redux/static/js/2.0728c676.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux/static/js/2.0728c676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5524a1f2638cad7284",
+    "url": "/redux/static/js/main.261d6a31.chunk.js"
+  },
+  {
+    "revision": "c42edd1bf56e39dae761",
+    "url": "/redux/static/js/runtime-main.e29d975d.js"
+  }
+]);
